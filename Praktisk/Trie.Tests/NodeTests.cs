fa8a1f250425c469f69737f01afa4a9d37fa76d7
Timur Barblishvili <unknitsplash@hotmail.com>
2018-11-28 23:56:03 +0300

    Add tests for removing nodes

--- Praktisk/Trie.Tests/NodeTests.cs
+++ Praktisk/Trie.Tests/NodeTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using FluentAssertions;
 using Xunit;
 
 namespace Trie.Tests
@@ -40,6 +39,39 @@ namespace Trie.Tests
 
             //ACT & CHECK
             Assert.Throws<ArgumentNullException>(() => parentNode.AddNode(null));            
+        }
+
+        [Theory]
+        [MemberData(nameof(NodeTestData))]
+        public void Test_RemoveNode_Removed(params object[] nodes)
+        {
+            //PREPARE
+            var childNodes = nodes.Select(n => (Node) n).ToList();
+            var parentNode = new Node('b', NodeType.Final);
+
+            foreach (var childNode in childNodes)
+            {
+                parentNode.AddNode(childNode);
+            }
+            
+            var nodeToRemove = childNodes.First();
+            childNodes.Remove(nodeToRemove);
+            
+            //ACT
+            parentNode.RemoveNode(nodeToRemove);
+            
+            //CHECK
+            parentNode.Children.ShouldBeEqualTo(childNodes); 
+        }
+        
+        [Fact]
+        public void Test_RemoveEmptyNode_ExceptionThrown()
+        {
+            //PREPARE
+            var parentNode = new Node('b', NodeType.Final);
+
+            //ACT & CHECK
+            Assert.Throws<ArgumentNullException>(() => parentNode.RemoveNode(null));            
         }
     }
 }
