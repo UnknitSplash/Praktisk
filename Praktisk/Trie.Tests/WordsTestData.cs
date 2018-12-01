f1c4bb49aa1fccdef2052649dcbc6e47670797f7
Timur Barblishvili <unknitsplash@hotmail.com>
2018-12-01 18:16:09 +0300

    Rename test data file to show the rounded amount of words

--- Praktisk/Trie.Tests/WordsTestData.cs
+++ Praktisk/Trie.Tests/WordsTestData.cs
@@ -6,13 +6,13 @@ namespace Trie.Tests
 {
     public class WordsTestData : IEnumerable<object[]>
     {
-        private static readonly string[] TestWords = File.ReadAllText("TestDataWords").Split('\n');
+        private static readonly string[] TestWords50K = File.ReadAllText("TestDataWords50k").Split('\n');
 
         public IEnumerable<object[]> Data => new List<object[]>
         {
             new object[] {new string[0]},
-            new object[] {new[] {"new", "old", "all", "always", "allocate", "new2"}},
-            new object[] {TestWords}
+            new object[] {new[] {"2", "new", "old", "all", "always", "allocate", "new2"}},
+            new object[] {TestWords50K}
         };
 
         public IEnumerator<object[]> GetEnumerator()
