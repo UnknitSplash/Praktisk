12729e7db60ab473f1869bbfe74e46c0a69fa6ab
Timur Barblishvili <unknitsplash@hotmail.com>
2018-12-01 03:47:43 +0300

    Optimise start of a search

--- Praktisk/Trie/Traversers/Traverser.cs
+++ Praktisk/Trie/Traversers/Traverser.cs
@@ -6,23 +6,14 @@ namespace Trie.Traversers
 {
     internal abstract class Traverser
     {
-        private readonly Node _root;
+        protected readonly Node _root;
 
         public Traverser(Node root)
         {
             _root = root;
         }
 
-        public IEnumerable<Node> Go()
-        {
-            foreach (var rootChildNode in _root)
-            {
-                foreach (var node in GetNodes(rootChildNode, 0))
-                {
-                    yield return node;
-                }
-            }
-        }
+        public abstract IEnumerable<Node> Go();
 
         protected abstract IEnumerable<Node> GetNodes(Node node, int depth);
     }
